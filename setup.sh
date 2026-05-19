b446a8b643a59a07a44ae96b0149f2761e803e12
Bayu Putra S <59677031+bps86@users.noreply.github.com>
2026-05-19 23:08:42 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-
+echo "Please add '&country=jp' in metalink of fedora repos"
+sleep 3
+sudo nano /etc/yum.repos.d/fedora.repo
+sudo nano /etc/yum.repos.d/fedora-updates.repo
 sudo dnf update
 sudo dnf group install development-tools
 sudo dnf install libmad-devel opusfile-devel
